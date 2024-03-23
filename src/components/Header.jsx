7546fb497d9f7acf3138dab48584ef8fc0df216e
Mirza Sahin <salihmirza123@gmail.com>
2024-03-23 20:40:49 +0300

    Add state functionality to Modal component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,33 @@
 import { MdPostAdd } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { modalFunc } from "../redux/modalSlice";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
   return (
     <div className="flex items-center justify-between bg-indigo-600 text-white px-4 py-3">
       <div className="text-2xl">REACT UYGULAMA</div>
 
-        <div className="flex items-center gap-5">
-                <div className="text-black">
-                    <select className="h-10 rounded-lg" name="" id="">
-                        <option value="asc">ARTAN</option>
-                        <option value="desc">AZALAN</option>
-                    </select>
-                </div>
-                    <input className="h-10 rounded-lg px-4" type="text" placeholder="Arama yapınız" />
-                <div className="bg-indigo-800 h-10 w-10 rounded-full flex items-center justify-center cursor-pointer">
-                    <MdPostAdd size={24} />
-                </div>
+      <div className="flex items-center gap-5">
+        <div className="text-black">
+          <select className="h-10 rounded-lg" name="" id="">
+            <option value="asc">ARTAN</option>
+            <option value="desc">AZALAN</option>
+          </select>
         </div>
-
+        <input
+          className="h-10 rounded-lg px-4"
+          type="text"
+          placeholder="Arama yapınız"
+        />
+        <div
+          onClick={() => dispatch(modalFunc())}
+          className="bg-indigo-800 h-10 w-10 rounded-full flex items-center justify-center cursor-pointer"
+        >
+          <MdPostAdd size={24} />
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
+import Modal from "../components/Modal";
 
 const Product = () => {
+  const { modal } = useSelector((state) => state.modal);
+
+  console.log('modal', modal);
   return (
     <div>
       <ProductCard />
+      {modal && <Modal />}
     </div>
   );
 };
